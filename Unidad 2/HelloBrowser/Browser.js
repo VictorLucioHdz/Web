15b9e18e459311c2153f48698b4b17f3d84f01c0
VictorLucioHdz <victorluciohdz0317@gmail.com>
2025-05-20 22:44:34 -0500

    Delete Unidad 2 directory

--- Unidad 2/HelloBrowser/Browser.js
+++
@@ -1,3 +0,0 @@
-function Buenas(){
-    alert('Buenas tardes')
-}
--- Unidad 2/HelloNode/Node.js
+++
@@ -1,9 +0,0 @@
-
-const cowsay = require("cowsay");
-console.log(cowsay.say({
-    text : "HOLA MUNDO ",
-    e : "oO",
-    T : "U "
-}));
-
-
